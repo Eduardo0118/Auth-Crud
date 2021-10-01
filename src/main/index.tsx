4d2e1d036e20618a58c47e312fb3ca09f1e5a075
Eduardo Jose <eduardo.carneiro@agilecontent.com>
2021-10-01 20:39:18 -0300

    fix: move global css

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-
 import { Router } from '@/presentation/components';
+import '@/presentation/styles/global.scss';
 
 render(<Router />, document.getElementById('main'));
