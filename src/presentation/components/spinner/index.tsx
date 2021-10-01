a30d079dc24bcacc422af0aac4f58a8aa093efac
Eduardo Jose <eduardo.carneiro@agilecontent.com>
2021-10-01 15:59:33 -0300

    feat: add login view

---
+++ src/presentation/components/spinner/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './styles.scss';
+
+type SpinnerProps = React.HtmlHTMLAttributes<HTMLElement>;
+
+const Spinner: React.FC<SpinnerProps> = (props: SpinnerProps) => {
+  return (
+    <div className={[styles.spinner, props.className].join(' ')}>
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+};
+
+export default Spinner;
