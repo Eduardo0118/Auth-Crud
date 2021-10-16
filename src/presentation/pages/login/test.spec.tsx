c1b8c4ce0851577216a661ca204e8cd212d40685
Eduardo Jose <eduardo.carneiro@agilecontent.com>
2021-10-15 21:37:22 -0300

    test: ensure Login present valid state if password is valid

--- src/presentation/pages/login/test.spec.tsx
+++ src/presentation/pages/login/test.spec.tsx
@@ -66,4 +66,15 @@ describe('Login component', () => {
     expect(passwordStatus.title).toBe(validationStub.errorMessage);
     expect(passwordStatus.textContent).toBe('🔴');
   });
+
+  test('Should show valid password state if Validation succeeds', () => {
+    const { sut, validationStub } = makeSut();
+    validationStub.errorMessage = null;
+    const passwordStatus = sut.getByTestId('password-status');
+    const passwordInput = sut.getByTestId('password');
+
+    fireEvent.input(passwordInput, { target: { value: internet.password() } });
+    expect(passwordStatus.title).toBe('Tudo certo');
+    expect(passwordStatus.textContent).toBe('🟢');
+  });
 });
