e264fcf15127351fff6e5e86645a787d3c103f4f
Eduardo Jose <eduardo.carneiro@agilecontent.com>
2021-11-19 16:52:38 -0300

    test: ensure Login disable submit button only if form is invalid

--- src/presentation/pages/login/test.spec.tsx
+++ src/presentation/pages/login/test.spec.tsx
@@ -88,4 +88,18 @@ describe('Login component', () => {
     expect(passwordStatus.title).toBe('Tudo certo');
     expect(passwordStatus.textContent).toBe('🟢');
   });
+
+  test('Should enable submit button if form is valid', () => {
+    const { sut, validationStub } = makeSut();
+    validationStub.errorMessage = null;
+
+    const emailInput = sut.getByTestId('email');
+    const passwordInput = sut.getByTestId('password');
+
+    fireEvent.input(emailInput, { target: { value: internet.email() } });
+    fireEvent.input(passwordInput, { target: { value: internet.password() } });
+
+    const submitButton = sut.getByTestId(/submit/i) as HTMLButtonElement;
+    expect(submitButton.disabled).toBe(false);
+  });
 });
